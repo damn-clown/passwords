0cb9301c757aafa635296a30b78b5deb49458c30
damn-clown <86931144+damn-clown@users.noreply.github.com>
2022-11-11 09:10:20 -0800

    Add files via upload

---
+++ password generator.py
@@ -0,0 +1,9 @@
+#passwords
+password_1 = input("Input a password here: ")
+password_2 = input("Re-enter password: ")
+
+if (password_1 == password_2):
+    print("Valid Password")
+
+else:
+    print("Invalid Password")
